ee0c08b7cb001e40c6fe12c68a5fce6fdd10febb
Chadwick Boulay <chadwick.boulay@gmail.com>
2015-11-04 13:23:28 -0500

    Made a test inlet to see what UE4 pushes to its outlet.

--- Tests/test_inlet.cpp
+++ Tests/test_inlet.cpp
@@ -1,5 +1,24 @@
 #include "lsl_cpp.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-	return 0;
+    
+    // resolve the stream of interest & make an inlet to get data from the first result
+    std::vector<lsl::stream_info> results = lsl::resolve_stream("type","Events");
+    lsl::stream_inlet inlet(results[0]);
+    lsl::stream_info info = inlet.info();
+    std::cout << "Found stream name " << info.name() << " of type " << info.type() << std::endl;
+    
+    // receive data & time stamps forever (not displaying them here)
+    float sample[1];
+    double ts;
+    while (true)
+    {
+        ts = inlet.pull_sample(&sample[0], 1);
+        std::cout << ts << "\t" << sample[0] << std::endl;
+    }
+    
+    
+    
+    return 0;
 }
