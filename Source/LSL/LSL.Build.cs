12cede16cfed5e525375d88bebceec54329a49db
Chadwick Boulay <chadwick.boulay@gmail.com>
2016-06-30 00:04:02 -0400

    Updated build rules. Works with 4.12.

--- Source/LSL/LSL.Build.cs
+++ Source/LSL/LSL.Build.cs
@@ -8,7 +8,7 @@ public class LSL : ModuleRules
 {
     private string ModulePath
     {
-        get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
+         get { return ModuleDirectory; }
     }
 
     private string ThirdPartyPath
