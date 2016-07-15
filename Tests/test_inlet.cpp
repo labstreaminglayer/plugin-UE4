a6a5dbc040933c139115eee6cf99aac75a01fac4
Chadwick Boulay <chadwick.boulay@gmail.com>
2016-07-15 18:33:55 -0400

    Updated test_inlet and test_outlet.

--- Tests/test_inlet.cpp
+++ Tests/test_inlet.cpp
@@ -4,18 +4,22 @@
 int main(int argc, char* argv[]) {
     
     // resolve the stream of interest & make an inlet to get data from the first result
-    std::vector<lsl::stream_info> results = lsl::resolve_stream("type","Events");
-    lsl::stream_inlet inlet(results[0]);
-    lsl::stream_info info = inlet.info();
-    std::cout << "Found stream name " << info.name() << " of type " << info.type() << std::endl;
-    
+	std::cout << "Searching for stream type Markers ..." << std::endl;
+    std::vector<lsl::stream_info> results = lsl::resolve_stream("type","Markers");
+	std::cout << "Found:" << std::endl;
+	std::cout << results[0].as_xml() << std::endl;
+
+	lsl::stream_inlet inlet(results[0]);
+	lsl::stream_info info = inlet.info();
+	std::cout << "Creating inlet for stream " << info.name() << " of type " << info.type() << std::endl;
+        
     // receive data & time stamps forever (not displaying them here)
-    float sample[1];
     double ts;
     while (true)
     {
-        ts = inlet.pull_sample(&sample[0], 1);
-        std::cout << ts << "\t" << sample[0] << std::endl;
+		std::string sample;
+        ts = inlet.pull_sample(&sample, 1);
+        std::cout << ts << "\t" << sample << std::endl;
     }
     
     
--- Tests/test_outlet.cpp
+++ Tests/test_outlet.cpp
@@ -1,44 +1,60 @@
 #include "lsl_cpp.h"
 #include <iostream>
-#include <time.h>
+#include <chrono>
 #include <stdlib.h>
+#include <thread>
 #include <math.h>
 
 #define NCHANNELS 1
+const char *channels[] = { "CONTROL1" };
+#define pi 3.14159
+
+double g_sine_freq = 0.5;
+double g_push_freq = 16;
 
 int main(int argc, char* argv[]) {
-	std::string name = "TestOutlet";
-	std::string type = "ControlSignal";
-    
-    int flip_flop = 1;
-	
+	std::string name = "SimpleOutlet";
+	std::string type = "Control";
+	auto start_time = std::chrono::high_resolution_clock::now();
+		
 	try {
-
-		// make a new stream_info (100 Hz)
-		lsl::stream_info info(name, type, NCHANNELS, 100, lsl::cf_float32, std::string(name) += type);
-
+		lsl::stream_info outlet_info(name, type, NCHANNELS, g_push_freq, lsl::cf_double64, std::string(name) += type);
+		// add some description fields
+		lsl::xml_element info_xml = outlet_info.desc();
+		lsl::xml_element channels_xml = outlet_info.desc().append_child("channels");
+		for (int k = 0; k < NCHANNELS; k++)
+		{
+			lsl::xml_element chn = channels_xml.append_child("channel");
+			chn.append_child_value("label", channels[k])
+				.append_child_value("unit", "SDU")
+				.append_child_value("type", "Generated");
+		}
 		// make a new outlet
-		lsl::stream_outlet outlet(info);
+		lsl::stream_outlet outlet(outlet_info);
 
 		// send data forever
 		std::cout << "Now sending data... " << std::endl;
-		double starttime = ((double)clock())/CLOCKS_PER_SEC;
-		for(unsigned t=0;;t++) {
-			
-			// wait a bit and create random data
-			while (((double)clock())/CLOCKS_PER_SEC < starttime + t*0.01);
-			float sample[NCHANNELS];
-			for (int c = 0; c < NCHANNELS; c++)
+		double push_counter = 0.0;
+		while(true) {
+			std::chrono::duration<double> elapsed = std::chrono::high_resolution_clock::now() - start_time;
+			double elapsed_seconds = elapsed.count();
+			if (elapsed_seconds > (push_counter / g_push_freq))
 			{
-                flip_flop = fmod(((double)clock())/CLOCKS_PER_SEC, 4) >= 2.0;
-                
-				// Random value between -0.5 and +0.5 or between +0.5 and +1.5
-				sample[c] = ((float)rand() / (float)RAND_MAX) - 0.5 + flip_flop;
+				double sample[NCHANNELS];
+				for (int c = 0; c < NCHANNELS; c++)
+				{
+					sample[c] = sin(2 * pi * g_sine_freq * elapsed_seconds);
+				}
+				// send the sample
+				outlet.push_sample(sample);
+				push_counter += 1.0;
+				std::cout << "Pushed sample (ch 1 = " << sample[0] << ")" << std::endl;
+			}
+			else
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(5));
 			}
 			
-			// send the sample
-			outlet.push_sample(sample);
-			std::cout << "Pushed sample (ch 1 = " << sample[0] << ")" << std::endl;
 		}
 
 	} catch(std::exception &e) {
