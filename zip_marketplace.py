abe6e1d66ef759ccf2e566d2ea9d1a33fa808781
Bertrand Richard <bertrand.richard@ifsttar.fr>
2022-04-13 09:18:17 +0200

    Added Python script to generate a Marketplace compliant archive

---
+++ zip_marketplace.py
@@ -0,0 +1,24 @@
+# Zips the plugin, keeping only required and valid files for Marketplace distribution
+import os
+import zipfile
+
+def zip(dest):
+    to_include = (
+        "Config",
+        "Resources",
+        "Source",
+        "LICENSE",
+        "LabStreamingLayer.uplugin"
+    )
+    with zipfile.ZipFile(dest, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk('.'):
+            for file in files:
+                relpath = os.path.relpath(os.path.join(root, file), '.')
+                if not relpath.startswith(to_include):
+                    continue
+                # Removing lslver.exe
+                if relpath.endswith(".exe"):
+                    continue
+                zipf.write(os.path.join(root, file), relpath)
+
+zip('UE4-LSL.zip')
